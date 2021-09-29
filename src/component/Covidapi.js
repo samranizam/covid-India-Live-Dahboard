52f0a0994e7b9cb503369cf024b639cf73082d03
Samra Nizan <66216751+samranizam@users.noreply.github.com>
2021-09-30 00:28:22 +0530

    Update Covidapi.js

--- src/component/Covidapi.js
+++ src/component/Covidapi.js
@@ -14,6 +14,7 @@ const Covid = () => {
              {
                   headers: {
                     "Access-Control-Allow-Origin":"*",
+                 
                   },
                 }
               )
